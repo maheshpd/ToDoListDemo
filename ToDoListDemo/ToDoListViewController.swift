3d765158af7bb0bd99344f64bbe5c647dc543bb9
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-05-27 03:33:45 +0530

    ToDoList TableView

--- ToDoListDemo/ToDoListViewController.swift
+++ ToDoListDemo/ToDoListViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 
-class ToDoListViewController: UIViewController {
+class ToDoListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var toDoItems: [ToDoItemModel] = [ToDoItemModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        
+        title = "To Do List"
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let toDoItem = toDoItems[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItem")!
+        
+        cell.textLabel?.text = toDoItem.name
+        cell.detailTextLabel?.text = toDoItem.isComplete ? "Complete" : "Incomplete"
+        
+        return cell
+    }
+    
 }
